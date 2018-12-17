6dfea6d666efe8a35449855580b8eed315b83658
DariaZvereva <zverevads@gmail.com>
2018-12-17 22:53:22 +0300

    MergeSortImpl task4 done

--- src/main/scala/lectures/collections/MergeSortImpl.scala
+++ src/main/scala/lectures/collections/MergeSortImpl.scala
@@ -7,6 +7,17 @@ package lectures.collections
   */
 object MergeSortImpl extends App {
 
-  def mergeSort(data: Seq[Int]): Seq[Int] = data
+  def merge(first: Seq[Int], second: Seq[Int]): Seq[Int] = (first, second) match {
+    case (x, Nil) => x
+    case (Nil, y) => y
+    case (xh :: xt, yh :: yt) => if (xh < yh) xh +: merge(xt, second) else yh +: merge(first, yt)
+  }
+
+  def mergeSort(data: Seq[Int]): Seq[Int] = data.length match {
+    case 0 | 1 => data
+    case n => val (left, right) = data.splitAt(n / 2); merge(mergeSort(left), mergeSort(right))
+  }
+
+  println(mergeSort(Seq(4,2,3,5)).mkString(","))
 
 }
